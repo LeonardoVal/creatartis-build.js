44766794b3eddeeac1f0684035690268e0b95a46
Leonardo Val <leonardo.val@creatartis.com>
2019-09-22 01:19:24 -0300

    Configure own build

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,4 +3,12 @@ const packageJSON = require('./package.json');
 
 Object.assign(exports, build.tasks({
   packageJSON,
+  esmFiles: false,
+  umdIndex: false,
+  copyFiles: [
+    { src: 'src/index.js', dest: 'dist/creatartis-build.js' },
+    { src: 'src/jest-setup.js', dest: 'dist/' },
+  ],
+  specFiles: false,
+  jsdocFiles: false,
 }));
