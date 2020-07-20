06dc1dbab54f0ba546964884fcad3af5022b230e
Leonardo Val <leonardo.val@creatartis.com>
2020-07-19 23:32:10 -0300

    Write configuration files on build
    Takes configuration in `package.json` and writes files on `dist/`,
    so they can be used by other packages.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,14 @@
+const fs = require('fs').promises;
 const build = require('./src/index');
 const packageJSON = require('./package.json');
+
+exports.configFiles = async function configFiles() {
+  const files = ['babel', 'eslintConfig', 'jest'];
+  await Promise.all(files.map((key) => (
+    fs.writeFile(`dist/${key}.json`,
+      JSON.stringify(packageJSON[key], null, '  '))
+  )));
+};
 
 Object.assign(exports, build.tasks({
   packageJSON,
@@ -9,5 +18,6 @@ Object.assign(exports, build.tasks({
     { src: 'src/index.js', dest: 'dist/creatartis-build.js' },
     { src: 'src/jest-setup.js', dest: 'dist/' },
   ],
+  afterBuild: [exports.configFiles],
   jsdocFiles: false,
 }));
