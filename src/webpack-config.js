59d18164dbd2a1b2000cb763e06a0c1418ac15a8
Leonardo Val <leonardo.val@creatartis.com>
2020-08-30 22:53:22 -0300

    Refactor Jest, JSDoc & Webpack configs

---
+++ src/webpack-config.js
@@ -0,0 +1,42 @@
+// eslint-disable-next-line import/no-dynamic-require
+const packageJSON = require(`${process.cwd()}/package.json`);
+
+const parsePackageName = (name) => {
+  const nameRegExp = /^(?:@([^/]+)\/)?(([^.]+)(?:\.(.*))?)$/;
+  const returnObject = {
+    scope: null,
+    fullName: '',
+    projectName: '',
+    moduleName: '',
+  };
+  const match = (typeof name === 'object' ? (name.name || '') : name || '')
+    .match(nameRegExp);
+  if (match) {
+    returnObject.scope = match[1] || null;
+    returnObject.fullName = match[2] || match[0];
+    returnObject.projectName = match[3] === match[2] ? null : match[3];
+    returnObject.moduleName = match[4] || match[2] || null;
+  }
+  return returnObject;
+};
+
+const PACKAGE_NAME = parsePackageName(packageJSON.name).fullName;
+
+module.exports = {
+  devtool: 'source-map',
+  mode: 'production',
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      use: ['babel-loader'],
+      exclude: /node_modules/,
+    }],
+  },
+  output: {
+    filename: `dist/${PACKAGE_NAME}.js`,
+    libraryTarget: 'umd',
+    // Workaround of a webpack bug: <https://github.com/webpack/webpack/issues/6784>.
+    globalObject: 'typeof self !== \'undefined\' ? self : this',
+  },
+  ...packageJSON.webpack,
+};
