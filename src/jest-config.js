5a17b52869eac3e44b287b8ae1fa858b280ceac1
Leonardo Val <leonardo.val@creatartis.com>
2020-09-06 00:32:50 -0300

    Collect coverage with Jest

--- src/jest-config.js
+++ src/jest-config.js
@@ -3,6 +3,10 @@ const path = require('path');
 const { jest } = require(`${process.cwd()}/package.json`);
 
 module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/'],
+  coverageDirectory: 'test/specs/coverage',
+  coverageReporters: ['json', 'text-summary'],
   rootDir: process.cwd(),
   setupFilesAfterEnv: [
     path.join(__dirname, 'jest-setup.js'),
