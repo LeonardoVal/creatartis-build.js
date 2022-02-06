d6237f7a3f7ae72ce39eb19d846e788963c705ea
Leonardo Val <leonardo.val@creatartis.com>
2022-02-06 00:37:29 -0300

    Release root (and not dist/)

--- src/tasks/tests.js
+++ src/tasks/tests.js
@@ -3,9 +3,7 @@ const { run } = require('./common');
 
 async function taskTest() {
   const configPath = path.join(__dirname, '../jest-config.js');
-  return run(`
-    npx jest ./test/specs --config ${configPath}
-  `);
+  return run(`npx jest ./test/specs --config ${configPath}`);
 }
 
 module.exports = {
