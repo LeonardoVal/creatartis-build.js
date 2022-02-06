9c29a2a273b0f8f67840f40f75d3f489a24e8ff4
Leonardo Val <leonardo.val@creatartis.com>
2022-02-06 00:36:58 -0300

    Choose either umd or esm build

--- src/tasks/builds.js
+++ src/tasks/builds.js
@@ -5,19 +5,14 @@ const { run } = require('./common');
 async function taskBuild(type) {
   await fs.rm('./dist', { force: true, recursive: true });
   await fs.mkdir('./dist');
-  let result = 0;
   if (!type || type === 'umd') {
-    result = await run(`
-      webpack ./src/index.js --config ${path.join(__dirname, '../webpack-config.js')}
-    `);
-    if (result !== 0) {
-      return result;
-    }
+    const configPath = path.join(__dirname, '../webpack-config.js');
+    return run(`npx webpack ./src/index.js --config ${configPath}`);
   }
-  if (!type || type === 'esm') {
-    result = await run('npx babel src/ --out-dir dist/ --source-maps=true');
+  if (type === 'esm') {
+    return run('npx babel src/ --out-dir dist/ --source-maps=true');
   }
-  return result;
+  throw new Error(`Unknown command build:${type}!`);
 }
 
 module.exports = {
