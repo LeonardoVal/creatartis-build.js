3b91673b4600e840e225ef3a2e8406e8d9890ea3
Leonardo Val <leonardo.val@creatartis.com>
2022-02-13 22:35:35 -0300

    Add rules to ESLint config
    Also remove plugin for React.

--- src/eslint-config.js
+++ src/eslint-config.js
@@ -9,13 +9,20 @@ module.exports = {
     document: 'readonly',
     window: 'readonly',
   },
+  ignorePatterns: ['dist/', 'docs/jsdoc/', 'node_modules/'],
   parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 2021,
   },
-  plugins: ['react', 'jest'],
+  plugins: ['jest'],
   rules: {
-    'class-methods-use-this': 0,
-    'no-underscore-dangle': 0,
+    'class-methods-use-this': 'off',
+    'no-await-in-loop': 'off',
+    'no-mixed-operators': 'off',
+    'no-nested-ternary': 'off',
+    'no-param-reassign': 'off',
+    'no-restricted-syntax': ['error', 'WithStatement', 'BinaryExpression[operator=\'in\']'],
+    'no-underscore-dangle': 'off',
+    'no-unused-vars': ['error', { vars: 'local', args: 'none' }],
   },
 };
